ba2aa6be27a114a11455fb3097af12a33fe4222d
- X D - <81095246+F0m14@users.noreply.github.com>
2023-09-14 23:24:42 +0300

    Add files via upload

--- challenges.js
+++ challenges.js
@@ -1,7 +1,14 @@
 const challengesContainer = document.getElementById('challenges-container');
 
 const challenges = [
-    {title: "Soon", description: "Coming Soon...", icon: "weekly.png"}
+    {title: "Challenge", description: "Bust through a closed door at different named locations (3)", icon: "icon_weekly.png"},
+    {title: "Challenge", description: "Hit an opponent within 10 seconds of hiding in a bush", icon: "icon_weekly.png"},
+    {title: "Challenge", description: "Travel distance in the air using item (750 METERS)", icon: "icon_weekly.png"},
+    {title: "Challenge", description: "Accept a job at job board in different matches (5)", icon: "icon_weekly.png"},
+    {title: "Challenge", description: "Deal damage to opponents from behind with SMG's or Assault Rifles (300 DAMAGE)", icon: "icon_weekly.png"},
+    {title: "Challenge", description: "Assist in defeating bosses (3 TIMES)", icon: "icon_weekly.png"},
+    {title: "Level Up", description: "Complete 5 WEEK 3 quest to unlock this week's trail and reward", icon: "icon_levelup.png"},
+    {title: "Level Up Challenge", description: "Hit an enemy player with rocket ram ground crash", icon: "icon_levelup.png"}
 ];
 
 challenges.forEach(challenge => {
